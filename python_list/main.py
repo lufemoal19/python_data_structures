9cf2ce270b70048c3794831b0cdaccbf4d970880
lufemoal19 <lufemoal19@gmail.com>
2023-07-22 20:24:33 -0600

    Update main.py

--- python_list/main.py
+++ python_list/main.py
@@ -5,3 +5,10 @@
 @since 7/22/2023
 """
 
+
+
+lista_aplicaciones = [] # 5 aplicaciones
+
+lista_notificaciones = [] # cantidad TOTAL de notificaciones
+
+lista_notificaciones_enviadas = [] # cantidad TOTAL de notificaciones enviadas
